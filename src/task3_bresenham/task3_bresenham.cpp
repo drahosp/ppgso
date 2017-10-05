4bb27bdc34a56f39c6171cf596c8239de8439018
Peter Drahoš <drahosp@gmail.com>
2017-10-05 17:09:34 +0200

    Added task3_bresenham.

---
+++ src/task3_bresenham/task3_bresenham.cpp
@@ -0,0 +1,45 @@
+// Task 3 - Implement Bresenham drawing alg.
+//        - Draw a star using lines
+//        - save as result.raw
+#include <fstream>
+#include <iostream>
+#include <vector>
+#include <cmath>
+
+#include <ppgso/ppgso.h>
+
+using namespace std;
+using namespace ppgso;
+
+// Size of the framebuffer
+const unsigned int SIZE = 512;
+
+struct Point {
+  int x,y;
+};
+
+// Bresenham drawing algorithm
+void drawLine(ppgso::Image& framebuffer, Point& from, Point& to) {
+  // TODO: Implement Bresenham drawing algorithm
+}
+
+int main()
+{
+  // Use ppgso::Image as our framebuffer
+  ppgso::Image framebuffer(SIZE, SIZE);
+
+  // TODO: Generate star points
+  vector<Point> points;
+
+  // Draw lines
+  for(unsigned int i = 0; i < points.size() - 1; i++)
+    drawLine(framebuffer, points[i], points[i+1]);
+
+  // Save the result
+  cout << "Generating task3_bresenham.bmp file ..." << endl;
+  image::saveBMP(framebuffer, "task3_bresenham.bmp");
+
+  cout << "Done." << endl;
+  return EXIT_SUCCESS;
+}
+
